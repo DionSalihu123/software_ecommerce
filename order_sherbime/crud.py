dcd6c3f21160a4a10d1632a35d4671d7893dfd76
DionSalihu123 <dionsalihu14@gmail.com>
2026-05-12 11:47:35 +0200

    order communicates (user,product) + validation (stable)

--- order_sherbime/crud.py
+++ order_sherbime/crud.py
@@ -1,13 +1,67 @@
 from sqlalchemy.orm import Session
+from fastapi import HTTPException
+import httpx
 
 from models import Order
 from schemas import OrderCreate
+
+
+USER_SERVICE_URL = "http://user-service:8000"
+PRODUCT_SERVICE_URL = "http://product-service:8000"
+
+
+def validate_user(user_id: int):
+
+    response = httpx.get(
+        f"{USER_SERVICE_URL}/users"
+    )
+
+    users = response.json()
+
+    for user in users:
+        if user["id"] == user_id:
+            return True
+
+    return False
+
+
+def validate_product(product_id: int):
+
+    response = httpx.get(
+        f"{PRODUCT_SERVICE_URL}/products/{product_id}"
+    )
+
+    return response.status_code == 200
 
 
 def create_order(
     db: Session,
     order: OrderCreate
 ):
+
+    # -----------------------------
+    # VALIDATE USER
+    # -----------------------------
+    if not validate_user(order.user_id):
+
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
+
+    # -----------------------------
+    # VALIDATE PRODUCT
+    # -----------------------------
+    if not validate_product(order.product_id):
+
+        raise HTTPException(
+            status_code=404,
+            detail="Product not found"
+        )
+
+    # -----------------------------
+    # CREATE ORDER
+    # -----------------------------
     new_order = Order(
         user_id=order.user_id,
         product_id=order.product_id
